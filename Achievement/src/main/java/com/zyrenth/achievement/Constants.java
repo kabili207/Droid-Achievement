6ce247d489f796eaac07f7a6b9baeb8cd5e58cc1
Andrew Nagle <kabili@zyrenth.com>
2013-06-04 22:51:18 -0400

    Converted project to use gradle build system.

---
+++ Achievement/src/main/java/com/zyrenth/achievement/Constants.java
@@ -0,0 +1,35 @@
+package com.zyrenth.achievement;
+
+/*
+ * Copyright 2011 Andrew Nagle
+ * 
+ * This file is part of Fortune Unlock.
+ *
+ * Fortune Unlock is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fortune Unlock is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Fortune Unlock.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+public final class Constants {
+	public static final String PREF_STRING = "AchievePrefs";
+
+	public static final String LOCATION = "location";
+	public static final String OFFSET = "offset";
+	public static final String X_SUFFIX = "_X";
+	public static final String Y_SUFFIX = "_Y";
+	public static final String OFFSETX = OFFSET + X_SUFFIX;
+	public static final String OFFSETY = OFFSET + Y_SUFFIX;
+	public static final String ENABLED = "enabled";
+	public static final String THEME = "theme";
+
+}
